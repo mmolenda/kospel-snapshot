7f8a065f72aa36a3d5e0a0f67c44feab2b9dec73
Marcin Molenda <spamm@molenda.eu>
2020-09-17 11:48:11 +0200

    enabling rclone

--- kospel.sh
+++ kospel.sh
@@ -3,5 +3,5 @@ OUTFILE="kospel-$(date +'%Y%m').csv"
 
 python3 $KOSPEL_DIR/kospel.py -v --username $1 --password $2 --outfile $KOSPEL_DIR/$OUTFILE >> $KOSPEL_DIR/kospel.log
 tail -50 $KOSPEL_DIR/kospel.log > $KOSPEL_DIR/kospel-tail.log
-#rclone copy $KOSPEL_DIR/$OUTFILE gdrive:Kospel/
-#rclone copy $KOSPEL_DIR/kospel-tail.log gdrive:Kospel/
+rclone copy $KOSPEL_DIR/$OUTFILE gdrive:Kospel/
+rclone copy $KOSPEL_DIR/kospel-tail.log gdrive:Kospel/
