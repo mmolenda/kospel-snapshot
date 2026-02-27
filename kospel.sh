0050dc823c7ccb5f5aca5279b47e3ca4fa515246
mmolenda <spamm@molenda.eu>
2026-02-27 14:03:41 +0100

    Fixing path, venv, remove influx push

--- kospel.sh
+++ kospel.sh
@@ -2,8 +2,8 @@ KOSPEL_DIR=$HOME/kospel-snapshot
 DATA_DIR=$HOME/export/radek
 OUTFILE="kospel-$(date +'%Y%m').csv"
 
-python3 $KOSPEL_DIR/kospel.py -v --username $1 --password $2 --outfile $DATA_DIR/$OUTFILE >> $DATA_DIR/kospel.log
-python3 $KOSPEL_DIR/kospel.py -v --username $1 --password $2 --outfile $DATA_DIR/kospel.json
+/root/kospel-snapshot/.venv/bin/python $KOSPEL_DIR/kospel.py -v --username $1 --password $2 --outfile $DATA_DIR/$OUTFILE >> $DATA_DIR/kospel.log
+/root/kospel-snapshot/.venv/bin/python $KOSPEL_DIR/kospel.py -v --username $1 --password $2 --outfile $DATA_DIR/kospel.json
 tail -50 $DATA_DIR/kospel.log > $DATA_DIR/kospel-tail.log
 tail -300 $DATA_DIR/$OUTFILE > /tmp/.kosp1
 # influx_push.py host org bucket token
